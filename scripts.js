2863b251927e42771a7b97ecca8491ce50f911ce
labkunz <bboykunz@gmail.com>
2020-08-21 20:21:11 +0800

    Clear scripts.js code and store.html delete cart-item in cart-all-item

--- scripts.js
+++ scripts.js
@@ -1,66 +1,67 @@
 //console.log("in here") //直接output來確認有抓到對應的html檔
 
-var addItemToCartButtons = document.getElementsByClassName('add-to-cart')
-//console.log(addItemToCartButtons) //確認有抓到相對應class的html標籤
-var removeCartItemButtons = document.getElementsByClassName('remove')
-var increaseQuantityButtons = document.getElementsByClassName('increase')
-var decreaseQuantityButtons = document.getElementsByClassName('decrease')
-var purchaseCartItemButton = document.getElementsByClassName('purchase')
-
-/*
-for(var i=0; i<addItemToCartButtons.length; i++){
-    var button = addItemToCartButtons[i]
-    button.addEventListener('click', function() {
-        console.log("clicked")
-    })
-    //button.addEventListener('click', addItemToCart()) //不能加上小括號,否則會在頁面載入時就觸發 WHY?
-}
-*/
-//上述是全寫在一function裡的方式
-
-//addItemToCartButtons.forEach(element => element.addEventListener('click', addItemToCart));
-//上述此方法無法使用
-Array.prototype.forEach.call(addItemToCartButtons, item => item.addEventListener('click', addToCartClicked))
-
-/*
-for(var i=0; i<removeCartItemButtons.length; i++){
-    var button = removeCartItemButtons[i]
-    button.addEventListener('click', removeCartItem)
-}
-*/
-//[removeCartItemButtons].forEach(item => item.addEventListener('click', removeCartItem))
-//上述此方法無法使用 WHY?
-
-for(var item of removeCartItemButtons){
-    item.addEventListener('click', removeCartItem)
+if(document.readyState == 'loading') { //網頁載入的狀態:讀取中(loading)
+    document.addEventListener('DOMContectLoaded', ready) 
+    //網頁生命週期中的DOMContectLoaded(已加載完HTML,DOM建構完成時)
+} else {
+    ready()
 }
 
-for(var i=0; i<increaseQuantityButtons.length; i++){
-    var button = increaseQuantityButtons[i]
-    button.addEventListener('click', increaseQuantity)
-}
+function ready() {
+    var addItemToCartButtons = document.getElementsByClassName('add-to-cart')
+    //console.log(addItemToCartButtons) //確認有抓到相對應class的html標籤
+    Array.prototype.forEach.call(addItemToCartButtons, item => item.addEventListener('click', addToCartClicked))
+    //上述是第一種forEach寫法
 
-for(var i=0; i<decreaseQuantityButtons.length; i++){
-    var button = decreaseQuantityButtons[i]
-    button.addEventListener('click', decreaseQuantity)
-}
+    var removeCartItemButtons = document.getElementsByClassName('remove')
+    for(var item of removeCartItemButtons){
+        item.addEventListener('click', removeCartItem)
+    }
+    //上述是第二種寫法,應該算for?
 
-purchaseCartItemButton[0].addEventListener('click', purchaseCartItem)
+    var increaseQuantityButtons = document.getElementsByClassName('increase')
+    for(var i=0; i<increaseQuantityButtons.length; i++){
+        var button = increaseQuantityButtons[i]
+        button.addEventListener('click', increaseQuantity)
+    }
+    //第三種,最常見的方式
+
+    var decreaseQuantityButtons = document.getElementsByClassName('decrease')
+    for(var i=0; i<decreaseQuantityButtons.length; i++){
+        var button = decreaseQuantityButtons[i]
+        button.addEventListener('click', decreaseQuantity)
+    }
+    /*
+    for(var i=0; i<addItemToCartButtons.length; i++){
+        var button = addItemToCartButtons[i]
+        button.addEventListener('click', function() {
+            console.log("clicked")
+        })
+        //button.addEventListener('click', addItemToCart()) //不能加上小括號,否則會在頁面載入時就觸發 WHY?
+    }
+    */
+    //上述是全寫在一function裡的方式
+
+    //[removeCartItemButtons].forEach(item => item.addEventListener('click', removeCartItem))
+    //上述此方法無法使用 WHY?
+
+    var purchaseCartItemButton = document.getElementsByClassName('purchase')
+    purchaseCartItemButton[0].addEventListener('click', purchaseCartItem)
+}
 
 function addToCartClicked(event) {
-    //console.log("Add to Cart button be clicked in function")
     var storeItem = event.target.parentNode.parentNode
+
     var storeItemTitle = storeItem.getElementsByClassName('store-goods')[0].innerText
-    var storeItemPrice = storeItem.getElementsByClassName('store-price')[0].innerText
-    storeItemPrice = storeItemPrice.replace('$','')
+    var storeItemPrice = storeItem.getElementsByClassName('store-price')[0].innerText.replace('$','')
     var storeItemImage = storeItem.getElementsByClassName('store-img')[0].src
     var storeItemIndex = storeItem.getAttribute('data-item')
-    //console.log(storeItemIndex)
 
     var allCartItem = document.getElementsByClassName('cart-all-item')[0].children
     var isNewItem = true;
     for(var i=0; i<allCartItem.length; i++) {
         if(allCartItem[i].getAttribute('data-item') == storeItemIndex) {
+        //利用HTML5新增的data-*屬性來確認購物車內是否已有此項目
             isNewItem = false;
             break
         }
@@ -76,7 +77,6 @@ function addToCartClicked(event) {
 }
 
 function addItemToCart(image, title, price, index) {
-
     var cartItem = `<div class="goods cart-column">  
                         <img class="goods-img" src="${image}" width="100" height="100">
                         <span class="goods-title"> ${title} </span>
@@ -102,45 +102,33 @@ function addItemToCart(image, title, price, index) {
     newItem.getElementsByClassName('increase')[0].addEventListener('click', increaseQuantity)
     newItem.getElementsByClassName('decrease')[0].addEventListener('click', decreaseQuantity)
     newItem.getElementsByClassName('remove')[0].addEventListener('click', removeCartItem)
-
-    //console.log(storeItemTitle)
-    //console.log(storeItemPrice)
-    //console.log(storeItemImage)
-    //console.log(storeItem)
-    console.log("finish add to cart")
+    //加上原本需要的事件
 }
 
 function removeCartItem(event) {
-    //console.log("Remove button be clicked in function")
     var cartItem = event.target.parentNode.parentNode
     cartItem.remove()
-    console.log("finish remove")
     totalCartItemPrice()
 }
 
 function increaseQuantity(event) {
-    //console.log("Increase button be clicked in function")
-    //var value = document.getElementsByClassName()
     var quantityField = event.target.parentNode
     var quantityInput = quantityField.getElementsByClassName('quantity-input')[0]
     var quantity = quantityInput.value
-    //console.log(value.parentNode.getElementsByClassName('quantity-input')[0].value)
-    //quantity += 1
+
     quantityInput.value = parseInt(quantity) + 1
-    console.log("finish increase quantity")
     singleCartItemPrice(quantityField, quantityInput.value)
     totalCartItemPrice()
 }
 
 function decreaseQuantity(event) {
-    //console.log("Decrease button be clicked in function")
     var quantityField = event.target.parentNode
     var quantityInput = quantityField.getElementsByClassName('quantity-input')[0]
     var quantity = quantityInput.value
+
     if(quantity > 1) {
-        quantityInput.value = parseInt(quantity) - 1
+        quantityInput.value = parseInt(quantity) - 1 //讓數量不要成為負數
     }
-    console.log("finish increase quantity")
     singleCartItemPrice(quantityField, quantityInput.value)
     totalCartItemPrice()
 }
@@ -148,6 +136,7 @@ function decreaseQuantity(event) {
 function singleCartItemPrice(itemTarget, quantity) {
     var item = itemTarget.parentNode
     var itemIndex = item.getAttribute('data-item')
+    
     var allStoreItem = document.getElementsByClassName('store-block')[0].children
     var singlePrice = "";
     for(var i=0; i<allStoreItem.length; i++) {
@@ -159,11 +148,8 @@ function singleCartItemPrice(itemTarget, quantity) {
     }
     
     var ItemPrice = parseInt(singlePrice) * quantity
-    //var goodPrice = document.getElementsByClassName('store-block')[0].children
     var currentlyPriceField = item.getElementsByClassName('price-title')[0]
     currentlyPriceField.textContent = " $" + ItemPrice + " "
-    //console.log(goodPrice)
-
 }
 
 function totalCartItemPrice() {
@@ -180,14 +166,15 @@ function totalCartItemPrice() {
 }
 
 function purchaseCartItem() {
-    //console.log("Purchase button be clicked in function")
     /*
     var allCartItem = document.getElementsByClassName('cart-all-item')[0].children
     var allCartItemNumber = allCartItem.length
     for(var i=0; i<allCartItemNumber; i++) {
         allCartItem[i].remove()
-    }
+    } 
     */
+    //上述為原本的寫法,執行起來會有不完全刪完的狀況
+
     var allCartItemField = document.getElementsByClassName('cart-all-item')[0]
     //allCartItemField.innerHTML = '' //第一種方法
 
@@ -197,5 +184,4 @@ function purchaseCartItem() {
 
     var totalPriceField = document.getElementsByClassName('totalPrice')[0]
     totalPriceField.innerText = " $0 "
-    console.log("finish purchase")
 }
